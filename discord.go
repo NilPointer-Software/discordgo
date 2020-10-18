e62f96d15ef18e887d7323f5722d3add4e2138e2
MineBartekSA <bartek_skoczen@protonmail.com>
2020-10-18 16:44:49 +0200

    Update discord.go

--- discord.go
+++ discord.go
@@ -21,7 +21,7 @@ import (
 )
 
 // VERSION of DiscordGo, follows Semantic Versioning. (http://semver.org/)
-const VERSION = "0.21.0"
+const VERSION = "0.2.0-nlp"
 
 // ErrMFA will be risen by New when the user has 2FA.
 var ErrMFA = errors.New("account has 2FA enabled")
